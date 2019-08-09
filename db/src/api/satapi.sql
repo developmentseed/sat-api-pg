3c1e9a969a10db8bbc19fa98e810bf797e78acc4
Sean Harkins <sharkinsgis@gmail.com>
2019-08-09 18:00:09 -0400

    Include exclude parameter handling.

--- db/src/api/satapi.sql
+++ db/src/api/satapi.sql
@@ -24,24 +24,41 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION searchwithfieldsfilter(bbox numeric[], include text)
+CREATE FUNCTION searchwithfieldsfilter(
+  bbox numeric[], include TEXT = NULL, exclude TEXT = NULL)
 RETURNS setof collectionitems
 AS $$
 DECLARE
 BEGIN
-  RETURN QUERY
-  SELECT collection,
-  collectionproperties,
-  id,
-  geom,
-  type,
-  assets,
-  geometry,
-  (select jsonb_object_agg(e.key, e.value)
-              from   jsonb_each(properties) e
-              where  e.key IN (include)) properties
-  FROM collectionitems 
-  WHERE data.ST_INTERSECTS(collectionitems.geom, data.ST_MakeEnvelope(bbox[1], bbox[2], bbox[3], bbox[4], 4326));
+  IF include IS NOT NULL THEN
+    RETURN QUERY
+    SELECT collection,
+    collectionproperties,
+    id,
+    geom,
+    type,
+    assets,
+    geometry,
+    (select jsonb_object_agg(e.key, e.value)
+                from   jsonb_each(properties) e
+                where  e.key IN (include)) properties
+    FROM collectionitems 
+    WHERE data.ST_INTERSECTS(collectionitems.geom, data.ST_MakeEnvelope(bbox[1], bbox[2], bbox[3], bbox[4], 4326));
+  ELSIF exclude IS NOT NULL THEN
+    RETURN QUERY
+    SELECT collection,
+    collectionproperties,
+    id,
+    geom,
+    type,
+    assets,
+    geometry,
+    (select jsonb_object_agg(e.key, e.value)
+                from   jsonb_each(properties) e
+                where  e.key NOT IN (exclude)) properties
+    FROM collectionitems 
+    WHERE data.ST_INTERSECTS(collectionitems.geom, data.ST_MakeEnvelope(bbox[1], bbox[2], bbox[3], bbox[4], 4326));
+  END IF; 
 END
 $$ LANGUAGE plpgsql;
 
