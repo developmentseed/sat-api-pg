5e8bbd34791c8692cf7b34973bc423b1d2326cb9
Sean Harkins <sharkinsgis@gmail.com>
2019-09-16 15:55:19 -0400

    Clean up autogenerated project files.

--- db/src/sample_data/reset.sql
+++ db/src/sample_data/reset.sql
@@ -2,7 +2,7 @@ BEGIN;
 \set QUIET on
 \set ON_ERROR_STOP on
 set client_min_messages to warning;
-truncate data.todo restart identity cascade;
-truncate data.user restart identity cascade;
+truncate data.collections restart identity cascade;
+truncate data.items restart identity cascade;
 \ir data.sql
 COMMIT;
