5aef8c6581f06486d9173483ca15cd9f790d8686
Sean Harkins <sharkinsgis@gmail.com>
2019-09-19 14:56:19 -0400

    Test restrcuturing.

--- tests/db/structure.sql
+++ tests/db/structure.sql
@@ -2,7 +2,9 @@ begin;
 select * from no_plan();
 
 select * from check_test(
-    views_are('api', array['collectionitems', 'items'], 'tables present' ),
+    views_are('api',
+    array['collectionitems', 'items', 'collections'],
+    'tables present' ),
     true,
     'all views are present in api schema',
     'tables present',
