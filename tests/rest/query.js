41cd7264e92ba0cd9ce708de7d527e886929025e
Sean Harkins <sharkinsgis@gmail.com>
2019-09-24 14:00:55 -0400

    Query extension tests.

---
+++ tests/rest/query.js
@@ -0,0 +1,60 @@
+import { restService, resetdb } from './common';
+import should from 'should'; // eslint-disable-line no-unused-vars
+
+describe('query extension', function () {
+  before(function (done) { resetdb(); done(); });
+  after(function (done) { resetdb(); done(); });
+
+  it('Uses collection properties and item properties for query', function (done) {
+    restService()
+      .post('search')
+      .send({
+        query: {
+          'eo:gsd': {
+            eq: 15
+          }
+        }
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+      .expect(r => {
+        r.body.features.length.should.be.above(1);
+        r.body.features[0].properties.should.not.have.property('eo:gsd');
+      });
+  });
+
+  it('Handles queries with no bbox or intersects', function (done) {
+    restService()
+      .post('search')
+      .send({
+        query: {
+          'eo:cloud_cover': {
+            eq: 26
+          }
+        }
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+      .expect(r => {
+        r.body.features.length.should.equal(1);
+      });
+  });
+
+  it('gte lte operators', function (done) {
+    restService()
+      .post('search')
+      .send({
+        query: {
+          'eo:cloud_cover': {
+            gte: 26,
+            lte: 40
+          }
+        }
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+      .expect(r => {
+        r.body.features.length.should.equal(1);
+      });
+  });
+});
