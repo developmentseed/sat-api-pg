31e0d3c1bc4859e6fe40e0cfd743a3d1a1480f22
Sean Harkins <sharkinsgis@gmail.com>
2019-09-24 13:38:53 -0400

    Include collections tests.

---
+++ tests/rest/collections.js
@@ -0,0 +1,33 @@
+import { restService } from './common';
+import landsat8l2Collection from './landsat8l2Collection.json';
+
+describe('collections', function () {
+  it('Initial insert of a collection returns 201', function (done) {
+    restService()
+      .post('collections')
+      .set('Prefer', 'return=minimal')
+      .set('Content-Type', 'application/json')
+      .withRole('application')
+      .send(landsat8l2Collection)
+      .expect(201, done);
+  });
+
+  it('Insert a collection without a valid JWT or role returns 401', function (done) {
+    restService()
+      .post('collections')
+      .set('Prefer', 'return=minimal')
+      .set('Content-Type', 'application/json')
+      .send(landsat8l2Collection)
+      .expect(401, done);
+  });
+
+  it('Inserting a collection with a duplicate id returns 409', function (done) {
+    restService()
+      .post('collections')
+      .set('Prefer', 'return=minimal')
+      .set('Content-Type', 'application/json')
+      .withRole('application')
+      .send(landsat8l2Collection)
+      .expect(409, done);
+  });
+});
