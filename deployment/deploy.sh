525807b9b30597a1b4c4fb29ca0b4a5201533c92
Sean Harkins <sharkinsgis@gmail.com>
2019-10-01 08:45:39 -0400

    Intermediate deployment updates.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,8 @@
 set -a # automatically export all variables
 source .env
 set +a
-aws cloudformation deploy --template-file cloudformation.yaml --stack-name \
+aws cloudformation deploy --template-file newCloudformation.yaml --stack-name \
 $STACK_NAME --tags Project=$PROJECT --parameter-overrides \
-DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASSWORD JwtSecret=$JWT_SECRET \
+DBSuperUser=$SUPER_USER DBSuperPassword=$SUPER_USER_PASSWORD \
+DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASS JwtSecret=$JWT_SECRET \
 --region $REGION --capabilities CAPABILITY_IAM
