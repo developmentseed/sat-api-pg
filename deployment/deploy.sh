d1eb76fbdcf7121624e1df01ca7546541a6494e1
Sean Harkins <sharkinsgis@gmail.com>
2019-10-01 14:56:29 -0400

    Include task definitions in CF.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,4 +6,5 @@ aws cloudformation deploy --template-file newCloudformation.yaml --stack-name \
 $STACK_NAME --tags Project=$PROJECT --parameter-overrides \
 DBSuperUser=$SUPER_USER DBSuperPassword=$SUPER_USER_PASSWORD \
 DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASS JwtSecret=$JWT_SECRET \
+OpenRestyImage=$OPEN_RESTY_IMAGE \
 --region $REGION --capabilities CAPABILITY_IAM
