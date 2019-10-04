2df857d59dae864ce8d90d1983c515f5d7df0997
Sean Harkins <sharkinsgis@gmail.com>
2019-10-04 17:01:59 -0400

    Manage Subzero migrations.

---
+++ deployment/createStack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -a # automatically export all variables
+source .env
+set +a
+aws cloudformation deploy --template-file newCloudformation.yaml --stack-name \
+$STACK_NAME --tags Project=$PROJECT --parameter-overrides \
+DBSuperUser=$SUPER_USER DBSuperPassword=$SUPER_USER_PASSWORD \
+DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASS JwtSecret=$JWT_SECRET \
+OpenRestyImage=$OPEN_RESTY_IMAGE \
+# DesiredCount=1 \
+--region $REGION --capabilities CAPABILITY_IAM \
--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,9 +2,8 @@
 set -a # automatically export all variables
 source .env
 set +a
-aws cloudformation deploy --template-file newCloudformation.yaml --stack-name \
-$STACK_NAME --tags Project=$PROJECT --parameter-overrides \
-DBSuperUser=$SUPER_USER DBSuperPassword=$SUPER_USER_PASSWORD \
-DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASS JwtSecret=$JWT_SECRET \
-OpenRestyImage=$OPEN_RESTY_IMAGE \
---region $REGION --capabilities CAPABILITY_IAM
+SQITCH_CMD=./sqitch
+aws ecr get-login --region $REGION --no-include-email | sh
+
+yarn subzero cloud deploy --dba $SUPER_USER --password $SUPER_USER_PASSWORD \
+--openresty-image-tag latest
