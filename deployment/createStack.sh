f6f829d0c5bc1d5ab359844cab5c4965aa25d9ab
Sean Harkins <sharkinsgis@gmail.com>
2019-10-07 11:03:14 -0400

    Use correct cloudformation.yml.

--- deployment/createStack.sh
+++ deployment/createStack.sh
@@ -2,7 +2,7 @@
 set -a # automatically export all variables
 source .env
 set +a
-aws cloudformation deploy --template-file newCloudformation.yaml --stack-name \
+aws cloudformation deploy --template-file cloudformation.yaml --stack-name \
 $STACK_NAME --tags Project=$PROJECT --parameter-overrides \
 DBSuperUser=$SUPER_USER DBSuperPassword=$SUPER_USER_PASSWORD \
 DBName=$DB_NAME DBUser=$DB_USER DBPassword=$DB_PASS JwtSecret=$JWT_SECRET \
