e185dfde7705722081d7e14fe9ae0596e8b111c6
Sean Harkins <sharkinsgis@gmail.com>
2019-10-17 17:20:33 -0400

    Update default collection fields.

--- openresty/lualib/user_code/defaultFields.lua
+++ openresty/lualib/user_code/defaultFields.lua
@@ -1,5 +1,5 @@
 module("defaultFields", package.seeall)
 local defaultFields = {}
 defaultFields.items = { "id", "collection", "geometry", "properties" ,"type" , "assets", "bbox", "links"}
-defaultFields.collections = { "id", "description", "properties" }
+defaultFields.collections = { "id", "description", "properties", "keywords", "version", "license", "providers", "links" }
 return defaultFields
