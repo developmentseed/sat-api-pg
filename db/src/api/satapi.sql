c977b17d4a8f9e3539114a12f9c0a921fe4600c0
Sean Harkins <sharkinsgis@gmail.com>
2019-11-13 14:11:30 -0500

    Update search dynamic SQL to use format for variable substitution.

--- db/src/api/satapi.sql
+++ db/src/api/satapi.sql
@@ -32,61 +32,53 @@ DECLARE
 BEGIN
  --  prefer := current_setting('request.header.prefer');
 RETURN QUERY EXECUTE
+FORMAT(
 'WITH g AS (
   SELECT CASE
   WHEN $1 IS NOT NULL THEN
-      data.ST_MakeEnvelope(
-        $1[1],
-        $1[2],
-        $1[3],
-        $1[4],
-        4326
-      )
-    WHEN $2 IS NOT NULL THEN
-      data.st_SetSRID(
-        data.ST_GeomFromGeoJSON($2),
-        4326
-      )
-    ELSE 
-      NULL
-    END AS geom
-)
-'
-|| 
-' SELECT
-    collectionproperties,
-    collection,
-    id,
-    c.geom,
-    c.bbox,
-    type,
-    assets,
-    geometry,
-    CASE WHEN $3 IS NULL THEN properties
-    ELSE (
-      SELECT jsonb_object_agg(e.key, e.value)
-      FROM jsonb_each(properties) e
-      WHERE e.key = ANY ($3)
+    data.ST_MakeEnvelope(
+      $1[1],
+      $1[2],
+      $1[3],
+      $1[4],
+      4326
     )
-    END as properties,
-    datetime,
-    links,
-    stac_version
-    FROM api.collectionitems c, g
-    WHERE (
-      g.geom IS NULL OR 
-      data.ST_Intersects(g.geom, c.geom)
-    )'
-||
-COALESCE(andQuery, '')
-||
-' ' || sort
-||
-' LIMIT ' || lim
-||
-' OFFSET ' || next
-|| ';'
+  WHEN $2 IS NOT NULL THEN
+    data.st_SetSRID(
+      data.ST_GeomFromGeoJSON($2),
+      4326
+    )
+  ELSE 
+    NULL
+  END AS geom
+)
+SELECT
+  collectionproperties,
+  collection,
+  id,
+  c.geom,
+  c.bbox,
+  type,
+  assets,
+  geometry,
+  CASE WHEN $3 IS NULL THEN properties
+  ELSE (
+    SELECT jsonb_object_agg(e.key, e.value)
+    FROM jsonb_each(properties) e
+    WHERE e.key = ANY ($3)
+  )
+  END as properties,
+  datetime,
+  links,
+  stac_version
+  FROM api.collectionitems c, g
+  WHERE (
+    g.geom IS NULL OR 
+    data.ST_Intersects(g.geom, c.geom)
+  ) %1s %2s LIMIT %3s OFFSET %4s;
+', COALESCE(andQuery, ''), sort, lim, next)
 USING bbox, intersects, include;
+
 res_headers := format('[{"Func-Range": "%s-%s/*"}]', next, lim::int - 1);
 PERFORM set_config('response.headers', res_headers, true);
 END;
