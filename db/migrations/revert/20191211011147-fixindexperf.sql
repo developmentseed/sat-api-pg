2a8ddb62297867236db4759c1bfaa6fd1c836def
Sean Harkins <sharkinsgis@gmail.com>
2019-12-10 20:17:14 -0500

    Fix search function structure to utilize geometry index.

---
+++ db/migrations/revert/20191211011147-fixindexperf.sql
@@ -0,0 +1,7 @@
+-- Revert sat-api-pg:20191211011147-fixindexperf from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ db/migrations/verify/20191211011147-fixindexperf.sql
@@ -0,0 +1,7 @@
+-- Verify sat-api-pg:20191211011147-fixindexperf on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
