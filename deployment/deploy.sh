f90ce661c787c1c63094a828f2cfcc0f390fc8d2
Anthony Lukach <anthonylukach@gmail.com>
2020-03-12 10:10:40 -0600

    Cleanup for legibility

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,7 +3,11 @@ set -a # automatically export all variables
 source .env
 set +a
 SQITCH_CMD=./sqitch
-aws ecr get-login --region $REGION --no-include-email | sh
+aws ecr get-login \
+  --region $REGION \
+  --no-include-email | sh
 
-yarn subzero cloud deploy --dba $SUPER_USER --password $SUPER_USER_PASSWORD \
---openresty-image-tag latest
+yarn subzero cloud deploy \
+  --dba $SUPER_USER \
+  --password $SUPER_USER_PASSWORD \
+  --openresty-image-tag latest
